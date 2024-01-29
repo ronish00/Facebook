597cc7a08dc21b901160ef2ac0735f6c43545d2b
ronishkatuwal00 <ronishkatuwal00@gmail.com>
2024-01-29 22:20:02 +0545

    feed ui completed

--- src/component/feed/Feed.jsx
+++ src/component/feed/Feed.jsx
@@ -1,11 +1,17 @@
 import './feed.css'
 import SharePost from "../sharepost/SharePost"
+import Post from '../post/Post'
 
 const Feed = () => {
   return (
     <div className="feed">
       <div className="feedWrapper">
         <SharePost />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
       </div>
     </div>
   )
--- src/component/post/Post.jsx
+++ src/component/post/Post.jsx
@@ -1,9 +1,35 @@
+import { MoreVert } from '@mui/icons-material'
 import './post.css'
 
 const Post = () => {
   return (
-    <div>
-      
+    <div className='post'>
+      <div className="postWrapper">
+        <div className="postTop">
+            <div className="postTopLeft">
+                <img className='postProfileImg' src="/assets/person/1.jpeg" alt="" />
+                <span className='postUsername'>John Doe</span>
+                <span className='postDate'>5 mins ago</span>
+            </div>
+            <div className="postTopRight">
+                <MoreVert />
+            </div>
+        </div>
+        <div className="postCenter">
+            <span className="postText">Hey! Its my first post:)</span>
+            <img className='postImg' src="assets/post/1.jpeg" alt="" srcset="" />
+        </div>
+        <div className="postBottom">
+            <div className="postButtonLeft">
+                <img className='likeIcon' src="assets/like.png" alt="" />
+                <img className='likeIcon' src="assets/heart.png" alt="" />
+                <span className="postLikeCounter">32 people like it</span>
+            </div>
+            <div className="postButtonRight">
+                <span className="postCommentText">9 comments</span>
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
